16dfd615e1c2af9790b59030737b545c41de03d5
shashirekhacserymec-pixel <shashirekha.cse.rymec@gmail.com>
2026-02-03 11:40:44 +0530

    Add files via upload

---
+++ day_2check1.py
@@ -0,0 +1,6 @@
+
+name = input("Enter your name: ")
+age = input("Enter your current age: ")
+age = int(age)
+age_in_2030 = age + 4
+print(f"Hey {name}, you will be {age_in_2030} years old in 2030!")
