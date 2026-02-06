0d62bd480836f0864050e534c48ed74ed05ec77c
Shashi Rekha Y <shashirekha.cse.rymec@gmail.com>
2026-02-06 11:25:40 +0530

    Delete internship_files directory

--- internship_files/day4_check3.py
+++
@@ -1,9 +0,0 @@
-
-friend_a = {"Python", "Cooking", "Hiking", "Movies"}
-friend_b = {"Hiking", "Gaming", "Photography", "Python"}
-common_interests = friend_a & friend_b          
-all_interests = friend_a | friend_b              
-unique_to_a = friend_a - friend_b                 
-print("Shared Interests:", common_interests)
-print("All Interests:", all_interests)
-print("Interests unique to Friend A:", unique_to_a)
